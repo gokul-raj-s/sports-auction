de6ce16a39bbd8d2b7604c44e215e4ee6595ef0b
Gokul Raj S <mscgokulraj@gmail.com>
2024-02-28 16:55:06 +0530

    test

--- auctiontool/src/main.js
+++ auctiontool/src/main.js
@@ -1,18 +1,1 @@
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
-
-import VueResource from 'vue-resource';
-
-Vue.use(VueResource);
-
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import Vue from 'vue'import BootstrapVue from 'bootstrap-vue'import 'bootstrap/dist/css/bootstrap.css'import 'bootstrap-vue/dist/bootstrap-vue.css'Vue.use(BootstrapVue)import VueResource from 'vue-resource';Vue.use(VueResource);import App from './App.vue'Vue.config.productionTip = false//testnew Vue({  render: h => h(App),}).$mount('
